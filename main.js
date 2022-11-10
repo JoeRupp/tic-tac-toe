fc03bab1b972b1e4aaddb2d587401b27d94e2cd2
Joe Rupp <joerpp8@gmail.com>
2022-11-09 17:17:49 -0700

    Add toggle functionality and reset score button functionality

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 let currentGame;
+let darkModeOn = false;
 
 const userScore = document.querySelector('.user-score');
 const cpuScore = document.querySelector('.cpu-score');
 const gameOutcome = document.querySelector('.outcome');
-const toggleSelection = document.querySelector('.option');
+const toggleUserX = document.querySelector('#X');
+const toggleUserO = document.querySelector('#O');
+const toggleLightMode = document.querySelector('#light');
+const toggleDarkMode = document.querySelector('#dark');
+const boardSection = document.querySelector('.board-section')
 const board00 = document.querySelector('.box-zero');
 const board01 = document.querySelector('.box-one');
 const board02 = document.querySelector('.box-two');
@@ -14,14 +19,51 @@ const board26 = document.querySelector('.box-six');
 const board27 = document.querySelector('.box-seven');
 const board28 = document.querySelector('.box-eight');
 const resetBtn = document.querySelector('.reset-btn');
-const playerBackgrounds = document.querySelector('.player')
-const boardBackgrounds = document.querySelector('main')
+const playerBackgrounds = document.querySelector('.player');
+const boardBackgrounds = document.querySelector('main');
 
-const startNewGame = () => {
+const makeNewGame = () => {
   currentGame = new Game();
-  console.log(currentGame)
 }
 
-startNewGame()
+const restartGame = () => {
+  currentGame.restartGame()
+}
 
-// EVENT LISTENERS -----------------------------------------
+const switchToDarkMode = () => {
+  
+}
+
+const togglePlayerSymbols = () => {
+  if (currentGame.user.token === 'X') {
+    currentGame.user.token = 'O';
+    toggleUserO.classList.remove('selected');
+    toggleUserX.classList.add('selected');
+  } else {
+    currentGame.user.token = 'X';
+    toggleUserO.classList.add('selected');
+    toggleUserX.classList.remove('selected');
+  }
+}
+
+const toggleLightDarkMode = () => {
+  if (darkModeOn) {
+    toggleLightMode.classList.add('selected');
+    toggleDarkMode.classList.remove('selected');
+    darkModeOn = false;
+  } else {
+    toggleLightMode.classList.remove('selected');
+    toggleDarkMode.classList.add('selected');
+    darkModeOn = true;
+    switchToDarkMode()
+  }
+}
+
+window.addEventListener('load', makeNewGame());
+
+// boardSection.addEventListener('click', );
+toggleUserX.addEventListener('click', togglePlayerSymbols);
+toggleUserO.addEventListener('click', togglePlayerSymbols);
+toggleLightMode.addEventListener('click', toggleLightDarkMode);
+toggleDarkMode.addEventListener('click', toggleLightDarkMode);
+resetBtn.addEventListener('click', restartGame);
